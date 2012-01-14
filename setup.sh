7b225273d3ca5eb7149d885d25277af2f9d8c34e
dean <dean@deanproxy.com>
2012-01-14 17:45:55 -0500

    link to gvimrc

--- setup.sh
+++ setup.sh
@@ -1,13 +1,23 @@
 #!/bin/sh
 
-directory="$(pwd)/$(dirname $0)"
+basedir=`dirname $0`
+if [ "$basedir" = "." ]; then
+    directory=`pwd`
+else
+    directory="$(pwd)/$basedir"
+fi
 
-ln -s "$directory/vim" "$HOME/.vim"
-ln -s "$directory/vim/vimrc" "$HOME/.vimrc"
-ln -s "$directory/zsh.zshrc" "$HOME/.zshrc"
-
-if [ `uname` = "Linux" ]; then
+if [ ! -h "$HOME/.vim" ]; then
+    ln -s "$directory/vim" "$HOME/.vim"
+fi
+if [ ! -h "$HOME/.vimrc" ]; then
+    ln -s "$directory/vim/vimrc" "$HOME/.vimrc"
+fi
+if [ ! -h "$HOME/.gvimrc" ]; then
     ln -s "$directory/vim/gvimrc" "$HOME/.gvimrc"
+fi
+if [ ! -h "$HOME/.zshrc" ]; then
+    ln -s "$directory/zsh.zshrc" "$HOME/.zshrc"
 fi
 
 # git setup
