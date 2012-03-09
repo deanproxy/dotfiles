ccd7931fbb0891b5c17fdc294166e7f76dea4fe0
dean <djones@ncircle.com>
2012-03-09 13:59:59 -0500

    some changes

--- xmonad/xmobar.hs
+++ xmonad/xmobar.hs
@@ -10,12 +10,12 @@ Config {
     lowerOnStart = True,
     commands = [
         Run Weather "KATL" ["-t","<tempF>F <skyCondition>","-L","64","-H","77","-n","#CEFFAC","-h","#FFB6B0","-l","#96CBFE"] 36000,
-        Run MultiCpu ["-t","Cpu: <total0> <total1>","-L","30","-H","60","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC","-w","3"] 10,
-        Run Memory ["-t","Mem: <usedratio>%","-H","8192","-L","4096","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC"] 10,
-        Run Date "%a %b %_d %l:%M" "date" 10,
+        Run MultiCpu ["-t","C:<total0>,<total1>","-L","30","-H","60","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC","-w","3"] 10,
+        Run Memory ["-t","M:<usedratio>%","-H","8192","-L","4096","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC"] 10,
+        Run Date "%a %b %_d %l:%M %p" "date" 10,
         Run StdinReader
     ],
     sepChar = "%",
     alignSep = "}{",
-    template = "%StdinReader% }{ %multicpu%   %memory%   %KATL%   <fc=#FFFFCC>%date%</fc>  "
+    template = "%StdinReader% }{ %multicpu% <fc=#FFB6B0>:</fc> %memory% <fc=#FFB6B0>:</fc> %KATL% <fc=#FFB6B0>:</fc> <fc=#FFFFCC>%date%</fc>  "
 }
