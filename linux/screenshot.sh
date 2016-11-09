4234c6c478ae8bce058166318606d550630c4ded
dean <dean@deanproxy.com>
2016-11-09 14:09:33 -0500

    fix xsel and font

--- linux/screenshot.sh
+++ linux/screenshot.sh
@@ -3,9 +3,11 @@
 #  - sudo apt install xsel scrot
 #  - sudo npm i -g imgur
 
+DISPLAY=:1
+
 if [ "$1" = "select" ]; then
    scrot -s -e 'imgur $f | xsel -b; rm -f $f'
 else
    scrot -e 'imgur $f | xsel -b; rm -f $f'
 fi
-i3-nagbar -t warning -m "Screenshot saved to clipboard."
+i3-nagbar -f "pango:Ubuntu Medium 11" -t warning -m "Screenshot saved to clipboard."
