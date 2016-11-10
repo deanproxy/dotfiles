fcf9025d2f22a90d7d623999a05243a85568954a
dean <dean@deanproxy.com>
2016-11-10 11:09:57 -0500

    fix multi screen

--- linux/docked.sh
+++ linux/docked.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 # Check to see if we're docked. If we are, switch monitors.
 
+export DISPLAY=:1
+export XAUTHORITY=/run/user/1000/gdm/Xauthority
+
 if (xrandr | grep "DP2-2 connected"); then
    xrandr --output DP2-2 --left-of eDP1 --auto --primary --output eDP1 --auto
 elif (xrandr | grep "DP1 connected"); then
    xrandr --output DP1 --left-of eDP1 --auto --primary --output eDP1 --auto
 else
-   xrandr --output DP1 --off --output DP2-2 --off --output eDP1 --auto
+   xrandr --output eDP1 --auto --primary --output DP1 --off --output DP2-2 --off 
 fi
 feh --bg-scale ~/Dropbox/Photos/Wallpapers/mountainbike1.jpg
