fea7e797059bff1813b50ab97785c14a3d515d82
dean <dean@deanproxy.com>
2016-11-10 10:40:23 -0500

    fix docking cause lenovo's dock station sucks

--- linux/docked.sh
+++ linux/docked.sh
@@ -2,5 +2,10 @@
 # Check to see if we're docked. If we are, switch monitors.
 
 if (xrandr | grep "DP2-2 connected"); then
-    /etc/acpi/external-monitor.sh dock
+   xrandr --output DP2-2 --left-of eDP1 --auto --primary --output eDP1 --auto
+elif (xrandr | grep "DP1 connected"); then
+   xrandr --output DP1 --left-of eDP1 --auto --primary --output eDP1 --auto
+else
+   xrandr --output DP1 --off --output DP2-2 --off --output eDP1 --auto
 fi
+feh --bg-scale ~/Dropbox/Photos/Wallpapers/mountainbike1.jpg
