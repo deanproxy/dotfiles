2ac86b73304aba1613f91741e6d801d3981d8a66
dean <dean.jones@codeblox.com>
2017-01-12 16:31:43 -0500

    update gtk css for gnome 3.20

--- linux/i3/scripts/weather.js
+++ linux/i3/scripts/weather.js
@@ -9,11 +9,26 @@ const client = rest.wrap(mime);
 
 const sunnyIcon = "";
 const cloudyIcon = "";
+const rainIcon = "";
+const snowIcon = "";
 
 client(`http://api.openweathermap.org/data/2.5/weather?zip=${ZIPCODE}&units=imperial&APPID=${APIKEY}`)
    .then(response => {
       const info = response.entity;
-      const icon = info.weather.main === 'clouds' ? cloudyIcon : sunnyIcon;
+      let icon = sunnyIcon;
+      switch (info.weather.main) {
+      case 'Clouds':
+         icon = cloudyIcon;
+         break;
+      case 'Rain':
+         icon = rainIcon;
+         break;
+      case 'Snow':
+         icon = snowIcon;
+         break;
+      default:
+         break;
+      }
       let output = `${icon} ${Math.round(info.main.temp)}F`;
       console.log(output);
    });
