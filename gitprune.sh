adea85ee03f035cd51bbea261b0e1dbab2140a7e
dean <dean.jones@codeblox.com>
2017-11-15 12:10:21 -0500

    fix up gitprune script

--- gitprune.sh
+++ gitprune.sh
@@ -1,31 +1,30 @@
 #!/bin/bash
 
-TEMP=$(getopt -o 'ps' -- "$@")
 exists=0
 yorn='y'
 
 git remote prune origin
 
-eval set -- "$TEMP"
-unset TEMP
-while true; do 
-   case "$1" in
-      '-p')
+while getopts ":psd:" o; do 
+   case "${o}" in
+      p)
          only_prune=1
-         shift
-         continue
          ;;
-      '-s')
+      s)
          only_stale=1
-         shift
-         continue
          ;;
-      '--')
-         shift
-         break
+      d)
+         stale_days=${OPTARG}
+         ;;
+      *)
+         ;;
    esac
 done
+shift $((OPTIND-1))
 
+if [ -z "$stale_days" ]; then
+   stale_days=5
+fi
 IFS=$'\n'
 
 # Prune branches
@@ -59,22 +58,50 @@ fi
 
 # Notify of possible stale branches.
 if [ -z $only_prune ]; then
+   branches=()
    today=`date +%s`
-   five_days=432000
+   total_days=$(expr $stale_days \* 24 \* 60 \* 60)
    for k in $(git branch -r | sed s/^..//); do 
       if [ "$k" != "origin/master" ] && [ "$k" != "origin/HEAD -> origin/master" ]; then
          date=`git log -1 --pretty=format:"%ci" $k --`
          secs=`date -d "${date}" +%s`
          diff=`expr $today - $secs`
-         if [ $diff -ge $five_days ]; then
+         if [ $diff -ge $total_days ]; then
             if [ -z $display ]; then
                echo
                echo "--- Possible stale branches ---"
                display=1
             fi
             days=`expr $diff / 60 / 60 / 24`
-            echo "$k is ${days} days old"
+            branches+=("${days}:${k}")
+            # echo "$k is ${days} days old"
          fi
       fi
    done
+
+   IFS=$'\n' sorted=($(sort -n <<<"${branches[*]}"))
+   for b in ${sorted[@]}; do
+      days=$(echo $b | awk -F: '{print $1}')
+      branch=$(echo $b | awk -F: '{print $2}')
+      echo "$branch is $days days old"
+   done
+   echo -n "Do you want to kill these? "
+   read yorn
+   if [ "$yorn" == "yes" ] || [ "$yorn" == "y" ]; then
+      echo -n "For realsies? "
+      read yorn
+      if [ "$yorn" == "yes" ] || [ "$yorn" == "y" ]; then
+         total=1
+         for b in ${sorted[@]}; do
+            days=$(echo $b | awk -F: '{print $1}')
+            branch=$(echo $b | awk -F: '{print $2}' | awk -F/ '{st=index($0,"/");print substr($0,st+1)}')
+            git push origin :"$branch"
+            total=$(expr $total + 1)
+            if [ $total -eq 30 ]; then
+               sleep 60
+               total=1
+            fi
+         done
+      fi
+   fi
 fi
