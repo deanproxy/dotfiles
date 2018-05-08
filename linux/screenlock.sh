0152932635b860e2f44bbdad81e3d118027e510b
dean <dean.jones@codeblox.com>
2018-05-08 15:25:13 -0400

    update lock script and i3

--- linux/screenlock.sh
+++ linux/screenlock.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-xset +dpms dpms 0 0 5
-i3lock -i <(import -silent -window root png:- | mogrify -blur 0x20 png:- | composite -gravity South -geometry -20x1200 ~/Scripts/rick.png png:- png:-)
-xset dpms 0 0 0
+if [ $((RANDOM % 2)) -eq 0 ]; then
+   nice -20 i3lock -i <(nice -20 import -silent -window root png:- | nice -20 mogrify -blur 0x20 png:- | nice -20 composite -gravity Center -geometry -20x1200 ~/Scripts/rick-explain.png png:- png:-)
+else
+   nice -20 i3lock -i <(nice -20 import -silent -window root png:- | nice -20 mogrify -blur 0x20 png:- | nice -20 composite -gravity South -geometry -20x1200 ~/Scripts/rick.png png:- png:-)
+fi
